ca3160b3a94cff92a6005bec8046e572df30c6a2
hamzaogounaine <hamz2487@gmail.com>
2024-11-05 10:46:15 +0100

    Watch servers changed with modifiying the navbar in mobile view

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,8 +1,3 @@
-/**
- * v0 by Vercel.
- * @see https://v0.dev/t/obus3LxsAfo
- * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
- */
 import { Link } from "react-router-dom";
 import {
   DropdownMenu,
@@ -14,38 +9,37 @@ import { Input } from "@/components/ui/input";
 import { Toggle } from "@/components/ui/toggle";
 import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet";
 import {
-  ArrowUpToLine,
   ChevronDownIcon,
-  List,
   LogOutIcon,
   MenuIcon,
   MoonIcon,
   SearchIcon,
   SunIcon,
   Tv,
-  User,
   User2,
 } from "lucide-react";
 import { useAuth } from "../contexts/authContext/authContext";
 import { logOut } from "../firebase/auth";
 import { useEffect, useState } from "react";
 import { useFirestore } from "../firebase/firestore";
+import { Accordion, AccordionItem, AccordionTrigger, AccordionContent } from "@/components/ui/accordion";
 
 export default function Navbar() {
   const { user, toggleMode, mode } = useAuth();
   const [watchlistItems, setWatchlistItems] = useState([]);
   const { getWatchList } = useFirestore();
+
   useEffect(() => {
     const fetchWatchList = async () => {
-
       const { movies, tvShows } = await getWatchList(user && user.uid);
       setWatchlistItems([...movies, ...tvShows]);
-    }
+    };
     fetchWatchList();
   }, [user, watchlistItems]);
+
   const logout = async () => {
     await logOut().then(() => {
-      console.log("lgoout");
+      console.log("Logged out");
     });
   };
 
@@ -58,7 +52,7 @@ export default function Navbar() {
             className="h-10 w-10"
           />
           <span className="sm:text-xl text-secondary-foreground font-[poppins]">
-            Fiml Guild
+            Film Guild
           </span>
         </Link>
         <nav className="hidden items-center gap-6 text-sm font-medium md:flex">
@@ -66,11 +60,11 @@ export default function Navbar() {
             to="/"
             className="text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
           >
-            <button>Home</button>
+            Home
           </Link>
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
-              <button className="flex  items-center gap-1 text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50">
+              <button className="flex items-center gap-1 text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50">
                 Movies
                 <ChevronDownIcon className="h-4 w-4" />
               </button>
@@ -107,7 +101,7 @@ export default function Navbar() {
           </DropdownMenu>
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
-              <button className="flex  items-center gap-1 text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50">
+              <button className="flex items-center gap-1 text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50">
                 TV series
                 <ChevronDownIcon className="h-4 w-4" />
               </button>
@@ -136,17 +130,15 @@ export default function Navbar() {
             </DropdownMenuContent>
           </DropdownMenu>
         </nav>
-        <div className="flex items-center gap-4 ">
-          <DropdownMenu className="">
+        <div className="flex items-center gap-4">
+          <DropdownMenu>
             <DropdownMenuTrigger asChild className="max-sm:hidden">
               <Button variant="ghost" size="icon" className="rounded-full">
-                <SearchIcon className="h-5 w-5  text-gray-500 dark:text-gray-400 " />
+                <SearchIcon className="h-5 w-5 text-gray-500 dark:text-gray-400" />
                 <span className="sr-only">Search</span>
               </Button>
             </DropdownMenuTrigger>
-            <DropdownMenuContent
-              className={`w-[300px] p-4 bg-background ${mode} `}
-            >
+            <DropdownMenuContent className={`w-[300px] p-4 bg-background ${mode}`}>
               <div className="relative bg-background">
                 <SearchIcon className="absolute left-2.5 top-2.5 h-4 w-4 text-gray-500 dark:text-gray-400" />
                 <Input
@@ -160,7 +152,7 @@ export default function Navbar() {
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
               <Button variant="ghost" size="icon" className="rounded-full">
-                <User2 className="h-5 w-5 text-gray-500  dark:text-gray-400" />
+                <User2 className="h-5 w-5 text-gray-500 dark:text-gray-400" />
               </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent className="p-4">
@@ -187,7 +179,6 @@ export default function Navbar() {
                   >
                     Profile
                   </Link>
-
                   <button
                     onClick={() => logout()}
                     className="flex gap-2 items-center text-sm font-medium text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
@@ -198,17 +189,14 @@ export default function Navbar() {
                 </div>
               )}
             </DropdownMenuContent>
-            <button className="p-3">
-              <Link
-                to="/watchlist"
-                className="text-sm relative font-medium text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
-              >
-                <Tv className="w-5 h-5" />
-                <span className="absolute bg-red-500 font-[poppins] h-3 w-3 -top-2 rounded-full flex items-center justify-center -right-2 text-xs text-white">{watchlistItems.length}</span>
-
-              </Link>
-            </button>
           </DropdownMenu>
+          <Link
+            to="/watchlist"
+            className="text-sm relative font-medium text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
+          >
+            <Tv className="w-5 h-5" />
+            <span className="absolute bg-red-500 font-[poppins] h-3 w-3 -top-2 rounded-full flex items-center justify-center -right-2 text-xs text-white">{watchlistItems.length}</span>
+          </Link>
           <Toggle
             aria-label="Toggle mode"
             className="rounded-full bg-background"
@@ -231,35 +219,77 @@ export default function Navbar() {
                 <span className="sr-only">Toggle navigation menu</span>
               </Button>
             </SheetTrigger>
-            <SheetContent
-              side="left"
-              className={`md:hidden bg-background ${mode}`}
-            >
+            <SheetContent side="left" className={`md:hidden bg-background ${mode}`}>
               <div className="grid gap-4 p-4">
                 <Link
-                  to="#"
-                  className="text-sm font-medium text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
+                  to="/"
+                  className="text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
                 >
-                  Home
+                  <button>Home</button>
                 </Link>
-                <Link
-                  to="#"
-                  className="text-sm font-medium text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
-                >
-                  About
-                </Link>
-                <Link
-                  to="#"
-                  className="text-sm font-medium text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
-                >
-                  Services
-                </Link>
-                <Link
-                  to="#"
-                  className="text-sm font-medium text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
-                >
-                  Contact
-                </Link>
+                <Accordion type="single" collapsible>
+                  <AccordionItem value="movies">
+                    <AccordionTrigger className="flex items-center gap-1 text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50">
+                      Movies
+                    </AccordionTrigger>
+                    <AccordionContent className="p-4 flex flex-col gap-1">
+                      <Link
+                        to="/movies"
+                        className="text-sm font-medium text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
+                      >
+                        All movies
+                      </Link>
+                      <hr className="border-gray-200 dark:border-gray-700" />
+                      <Link
+                        to="/top-rated-movies"
+                        className="text-sm font-medium text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
+                      >
+                        Top rated movies
+                      </Link>
+                      <hr className="border-gray-200 dark:border-gray-700" />
+                      <Link
+                        to="/ARmovies"
+                        className="text-sm font-medium text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
+                      >
+                        Arabic movies
+                      </Link>
+                      <hr className="border-gray-200 dark:border-gray-700" />
+                      <Link
+                        to="/animation"
+                        className="text-sm font-medium text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
+                      >
+                        Animation movies
+                      </Link>
+                    </AccordionContent>
+                  </AccordionItem>
+                  <AccordionItem value="tvseries">
+                    <AccordionTrigger className="flex items-center gap-1 text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50">
+                      TV series
+                    </AccordionTrigger>
+                    <AccordionContent className="p-4 flex flex-col gap-1">
+                      <Link
+                        to="/tvseries"
+                        className="text-sm font-medium text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
+                      >
+                        All TV series
+                      </Link>
+                      <hr className="border-gray-200 dark:border-gray-700" />
+                      <Link
+                        to="/top-rated-tv-shows"
+                        className="text-sm font-medium text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
+                      >
+                        Top rated series
+                      </Link>
+                      <hr className="border-gray-200 dark:border-gray-700" />
+                      <Link
+                        to="/mini-series"
+                        className="text-sm font-medium text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
+                      >
+                        Mini series
+                      </Link>
+                    </AccordionContent>
+                  </AccordionItem>
+                </Accordion>
               </div>
             </SheetContent>
           </Sheet>
--- src/pages/watch/TvShow.jsx
+++ src/pages/watch/TvShow.jsx
@@ -27,7 +27,7 @@ const TvWatch = () => {
     }, [])
     useEffect(() => {
         window.scrollTo(0, 0)
-        
+
     })
     useEffect(() => {
         getTvShowEpisodes(id, season).then(data => {
@@ -35,10 +35,26 @@ const TvWatch = () => {
             console.log(data)
         })
     }, [season])
-    const handleSortChange = (value) => {
+    const handleServerChange = (value) => {
         if (value === 'vidsrc.dev') {
             setBaseUrl('https://vidsrc.me/embed/tv');
         }
+        if (value === 'embed.su') {
+            setBaseUrl('https://embed.su/embed/tv');
+        }
+        if (value === 'vidsrc.icu') {
+            setBaseUrl('https://vidsrc.icu/embed/tv');
+        }
+        if (value === 'vidlink.pro') {
+            setBaseUrl('https://vidlink.pro/embed/tv');
+        }
+        if (value === 'vidsrc.net') {
+            setBaseUrl('https://vidsrc.net/embed/tv');
+        }
+        if (value === '2embed.cc') {
+            setBaseUrl('https://www.2embed.cc/embedtv');
+        }
+       
     }
     const handleSeasonChange = (value) => {
         navigate(`/tv/watch/${id}/${value}/1`)
@@ -50,11 +66,11 @@ const TvWatch = () => {
                     <Loader className='animate-spin text-foreground' /> please wait
                 </div>
                 <iframe
-                    src={`${base_url}/${id}/${parseInt(season)}/${parseInt(episode)}`}
+                    src={base_url.includes('2embed.cc') ? `${base_url}/${id}&s=${season}&e=${episode}` :`${base_url}/${id}/${parseInt(season)}/${parseInt(episode)}`}
                     allowFullScreen
                     scrolling="no"
                     width="100%"
-                
+
                     style={{ border: "none" }}
                     title="Embedded Video"
                     className='rounded-md mt-3 z-10 max-sm:h-[200px] h-[500px]'
@@ -63,27 +79,26 @@ const TvWatch = () => {
             <div>
 
                 <div className='py-3 flex sm:gap-10 gap-4'>
-                    <Select onValueChange={handleSortChange} className='bg-background w-full'>
+                    <Select onValueChange={handleServerChange} className='bg-background w-full'>
                         <SelectTrigger className="w-full  bg-background text-foreground">
                             <SelectValue placeholder="Select a service" className='text-foreground' />
                         </SelectTrigger>
                         <SelectContent>
-                            <SelectItem value="vidsrc.dev">vidsrc.dev</SelectItem>
                             <SelectItem value="embed.su">embed.su</SelectItem>
+                            <SelectItem value="vidsrc.dev">vidsrc.dev</SelectItem>
                             <SelectItem value="vidsrc.icu">vidsrc.icu</SelectItem>
                             <SelectItem value="vidlink.pro">vidlink.pro</SelectItem>
                             <SelectItem value="vidsrc.net">vidsrc.net</SelectItem>
                             <SelectItem value="2embed.cc">2embed.cc</SelectItem>
-                            <SelectItem value="vidsrc.vip">vidsrc.vip</SelectItem>
                         </SelectContent>
                     </Select>
-                    <Select onValueChange={handleSeasonChange}  className='bg-background w-full'>
+                    <Select onValueChange={handleSeasonChange} className='bg-background w-full'>
                         <SelectTrigger className="w-full  bg-background text-foreground">
                             <SelectValue placeholder="Select a season" className='text-foreground' />
                         </SelectTrigger>
                         <SelectContent>
                             {seasons && seasons.map(season => (
-                                <SelectItem  value={season.season_number}><Link to={`/tv/watch/${season.season_number}/1`}>{season.name}</Link></SelectItem>
+                                <SelectItem value={season.season_number}><Link to={`/tv/watch/${season.season_number}/1`}>{season.name}</Link></SelectItem>
                             ))}
                         </SelectContent>
                     </Select>
@@ -92,29 +107,29 @@ const TvWatch = () => {
                 <div className="flex sm:gap-10 gap-4 mb-3">
                     <Button className='w-full' disabled={parseInt(episode) === 1} variant=''><Link to={`/tv/watch/${id}/${season}/${parseInt(episode) > 1 ? parseInt(episode) - 1 : 1}`}>Previous episode</Link></Button>
                     <Button className='w-full' disabled={parseInt(episode) === episodes.length} variant=''><Link to={`/tv/watch/${id}/${season}/${parseInt(episode) + 1}`}>Next episode</Link></Button>
-                    
+
                 </div>
                 <h1 className='text-lg mb-4 '>Select an episode :</h1>
-                    <ScrollArea className="flex flex-col max-h-[400px]">
-                        {episodes && episodes.map(ep => (
-                            <div key={ep.id} className={`flex gap-4 p-3 mb-2 rounded-md border ${ ep.episode_number == (episode) ? 'bg-slate-300 text-black' : 'bg-background'}`}>
-                                <div className='relative flex items-center'>
-                                    <img src={`${image_base_url}${ep.still_path}`}  className='rounded max-w-52 max-sm:max-w-24 max-h-24' alt={ep.name}/>
-                                    <Link to={`/tv/watch/${id}/${ep.season_number}/${ep.episode_number}`} className='absolute inset-0'>
+                <ScrollArea className="flex flex-col max-h-[400px]">
+                    {episodes && episodes.map(ep => (
+                        <div key={ep.id} className={`flex gap-4 p-3 mb-2 rounded-md border ${ep.episode_number == (episode) ? 'bg-slate-300 text-black' : 'bg-background'}`}>
+                            <div className='relative flex items-center'>
+                                <img src={`${image_base_url}${ep.still_path}`} className='rounded max-w-52 max-sm:max-w-24 max-h-24' alt={ep.name} />
+                                <Link to={`/tv/watch/${id}/${ep.season_number}/${ep.episode_number}`} className='absolute inset-0'>
                                     <Play className='cursor-pointer absolute inset-0 m-auto flex justify-center items-center bg-slate-800 bg-opacity-30 rounded-full p-2 hover:scale-110 transition-all text-white' size={30} />
                                     <span className='absolute left-0 bottom-0 bg-zinc-700 border-opacity-20 text-white items-center text-sm px-1 justify-center flex rounded-tr-md min-w-5 text-center'>{ep.episode_number}</span>
-                                    </Link>
-                                </div>
-                                <div className='flex flex-col gap-1'>
-                                    <h1 className='max-sm:text-xs'>{ep.name}</h1>
-                                    <p className='text-sm  max-sm:text-[10px] max-sm:leading-3  sm:break-words max-sm:line-clamp-2' >{ep.overview}</p>
-                                    </div>
+                                </Link>
                             </div>
-                        ))}
-                        <ScrollBar orientation="vertical" className="bg-foreground rounded" />
-                    </ScrollArea>
-                </div>
+                            <div className='flex flex-col gap-1'>
+                                <h1 className='max-sm:text-xs'>{ep.name}</h1>
+                                <p className='text-sm  max-sm:text-[10px] max-sm:leading-3  sm:break-words max-sm:line-clamp-2' >{ep.overview}</p>
+                            </div>
+                        </div>
+                    ))}
+                    <ScrollBar orientation="vertical" className="bg-foreground rounded" />
+                </ScrollArea>
             </div>
+        </div>
     )
 }
 
